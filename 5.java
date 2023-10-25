8098e0e27a4613c7fe6665051bb3f53a953899a7
Rita Fasolino <84328521+rfasolino@users.noreply.github.com>
2023-10-25 12:01:50 -0300

    Update 5.java

--- 5.java
+++ 5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; 
- */  /**/
- public class ejer5 { 
+
+public class ejer5 { 
   
          public static void main(String[] args) { 
                  // TODO Auto-generated method stub 
